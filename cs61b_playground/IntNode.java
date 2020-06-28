b246b4a18ad81e230a8075968e99accd8c2338de
Lance Suen <10292888+LanceSuen@users.noreply.github.com>
2020-06-28 15:08:50 +0800

    Horse question

--- cs61b_playground/IntNode.java
+++
@@ -1,8 +0,0 @@
-public class IntNode{
-    public int item;
-    public IntNode first;
-    public IntNode(int x, IntNode y){
-        this.item = x;
-        this.first = y;
-    }
-}
--- cs61b_playground/SLList.java
+++ cs61b_playground/SLList.java
@@ -1,23 +1,53 @@
 public class SLList {
-    IntNode first;
+    public IntNode first; /** the first may be a pointer. */
 
+    public class IntNode{
+        public int item;
+        public IntNode next;
+        public IntNode(int x, IntNode y){
+            this.item = x;
+            this.next = y;
+        }
+    }
     public SLList(int x){
-        IntNode first = new IntNode(x, null);
+        this.first = new IntNode(x, null);
+    /** Why IntNode first = new IntNode(x, null); is wrong? 
+        We do not need a new declaration in the construction function.
+        If a new first is declared, first in the object of SLList will be null*/
     }
 
-    public void addfirst(int x){
+    public void addLast(int x){
+        IntNode p;
+        p = this.first;
+        while (p != null){
+            p = p.next; /** very important */
+        }
+        p = new IntNode (x, null);
+    }
+    public int size(){
+        IntNode p;
+        int listSize = 1;
+        p = this.first;
+        while (p != null){
+            listSize = listSize + 1;
+            p = p.next;
+            System.out.println("moving");
+        }
+        return listSize;
+    }
+
+    public void addFirst(int x){
         this.first = new IntNode(x, this.first);
     }
-    public int getfirst(){
+    public int getFirst(){
         return this.first.item;
     }
     public static void main(String[] args){
         SLList L = new SLList(1);
-        L.addfirst(2);
-        L.addfirst(3);
-        L.addfirst(4);
-        L.addfirst(5);
-        System.out.println(L.getfirst());
+        L.addFirst(2);
+        L.addFirst(3);
+        L.addLast(1);
+        System.out.println(L.size());
     }
     
 }
